a04edaf8c7288a5d2fdfd18b10850342718a28b0
Eugene Mutai <eugenemutai@gmail.com>
2015-06-18 10:45:19 +0300

    corrected my documentation

--- array_shuffle.js
+++ array_shuffle.js
@@ -2,7 +2,7 @@ Array.prototype.shuffleMe = function() {
     // Get the length
     var array_length = this.length;
 
-    // during looping it will start with the length ofr th array
+    // during looping it will start with the length of the array
     // which is the highest value of the array
     var loop_no = array_length;
     while (--loop_no > 0) {
@@ -10,19 +10,20 @@ Array.prototype.shuffleMe = function() {
         var random_number = Math.floor(Math.random() * (loop_no + 1));
         // console.log(random_number);
 
-        // get the value of the array in a random no.
+        // get the value of the array in the random no position.
         var value = this[random_number];
 
-        // now re-assign the array position above above with a value
+        // now re-assign the array value above with a value
         // from the same array, value provided by loop_no position
         this[random_number] = this[loop_no];
 
-        // viceversa
+        // now give the value variable to the array position thar replaced it
         this[loop_no] = value;
     }
 
     return this;
 }
+
 var array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 var result = array.shuffleMe();
 console.log(result);

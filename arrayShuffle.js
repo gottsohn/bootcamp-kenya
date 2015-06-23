c3033a882ead2a532227299005e586a0b403947f
mwalex <alexmuturi@gmail.com>
2015-06-23 13:05:35 +0300

    Update arrayShuffle.js

--- arrayShuffle.js
+++ arrayShuffle.js
@@ -4,17 +4,17 @@ Array.prototype.shuffle=function(){
            while (--i > 0) {
 	           
 		           j=Math.floor(Math.random()*(i));
-			           temp=this[j];// takes random position in temporary 
-				          this[j]=this[i];// 
-					         this[i]=temp;
+			     temp=this[j];// takes random position in temporary 
+				  this[j]=this[i];// 
+				this[i]=temp;
 						         
 							     }
-							         return this;
+				 return this;
 								     
 								         
-									 }
+				 }
 
-									 var array =['A','B','C','D','E','F','G'];
-									 var result=array.shuffle();
+				 var array =['A','B','C','D','E','F','G'];
+				 var result=array.shuffle();
 
-									 console.log(result);
+				 console.log(result);

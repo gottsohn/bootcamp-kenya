f83fb75b8c4f9a6d8505c2006588e47affa93298
natemmartin <godmetweenciati@gmail.com>
2015-06-23 08:18:38 +0300

    added the jquery folder

---
+++ hello-jquery/getting_imgs_with_src.js
@@ -0,0 +1,22 @@
+var imgs = jQuery("img");
+imgs.forEach(function(v){ 
+	var 
+if (imgs.attr("src")){
+	imgSrcArray.push(jQuery(imgs));
+	return imgSrcArray.length;
+}
+});
+
+
+var imgs = $("img"); 
+var images = []; 
+$.each(imgs, function(k,v){ 
+	images.push(
+		$(v).attr("src")
+		);
+});
+
+
+var imgs = $("img");
+$("img").[0] //first element, image in the array $("img")
+$("img").get(0) //also expressed as

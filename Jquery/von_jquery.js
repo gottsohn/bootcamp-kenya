9799d9bfe588f216c12aaa275229dd8ad7ef81d2
unknown <alexmuturi@gmail.com>
2015-06-23 13:00:07 +0300

    Jquery von.js

--- Jquery/von_jquery.js
+++ Jquery/von_jquery.js
@@ -1,9 +1,23 @@
 $=jQUery;
+var imgs = jQuery("img");
+imgs.forEach(function(v){ 
+	var 
+if (imgs.attr("src")){
+	imgSrcArray.push(jQuery(imgs));
+	return imgSrcArray.length;
+}
+});
 
 
-	var imgs =$('img');
+var imgs = $("img"); 
+var images = []; 
+$.each(imgs, function(k,v){ 
+	images.push(
+		$(v).attr("src")
+		);
+});
 
-		$.each(imgs,function(k,v) {
-			images.push($v.attr('src'));
-					})
 
+var imgs = $("img");
+$("img").[0] ("img")
+$("img").get(0)
